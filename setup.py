6be5da4207ea4d69fcde0da63b3a54da1699f557
Toshiaki Baba <toshiaki@netmark.jp>
2015-12-08 18:53:42 +0900

    add setup.py for pypi

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="nagiosctl",
+    version="1.0.0",
+    description="control multiple nagios",
+    author="Toshiaki Baba",
+    author_email="toshiaki@netmark.jp",
+    url="https://github.com/netmarkjp/nagiosctl",
+    install_requires=["click", "paramiko", "PyYAML", "requests", ],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Topic :: System :: Monitoring",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+    ],
+    scripts=["bin/nagiosctl", ],
+)
